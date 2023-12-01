50759424f9fc524eb8346005cd4d91083b84a619
Cormac Mangan <g00405662@atu.ie>
2023-12-01 12:27:50 +0000

    lab9

--- src/components/books.js
+++ src/components/books.js
@@ -2,9 +2,11 @@ import BookItem from "./bookItem";
 
 function Books(props){
 
+
+    // Passing the book data as a prop
     return props.myBooks.map(
         (book)=>{
-            return <BookItem myBook={book} key={book._id}></BookItem>
+            return <BookItem myBook={book} key={book._id} Reload={()=>{props.ReloadData();}}></BookItem>
         }
     );
 
